16c760b5f09f00175b5f745033b7643f7f77fc7c
Mudit <mudit.stja@gmail.com>
2020-09-10 04:27:52 +0530

    phase-24: issue - No other type or embed videos play

--- public/app-view/pages/pagesController.js
+++ public/app-view/pages/pagesController.js
@@ -399,6 +399,9 @@ app.controller("AdspagesController",['$scope','$rootScope','$location','HttpServ
         if(embed.indexOf('storage.googleapis.com') > -1) {
             $scope.iframe = false;
         } else if (embed.indexOf('<iframe') > -1) {
+            let embedArr = currentPost.embed.split('xxx="');
+            embedArr = embedArr[1].split('"');
+            embed = embedArr[0];
             $scope.iframe = true;
         } else {
           $scope.iframe = false;
